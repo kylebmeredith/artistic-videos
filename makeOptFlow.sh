4a70a4ba8565804f439de865d4310dec2cc6367f
NameRX <apxubatop@mail.ru>
2017-01-19 00:01:42 +0300

    makeOptFlow.sh change for creating long-term flow
    makeOptFlow.sh changes:
    -stepSize argument change: The step size can now be set to create long-term flow. May be an array, similar to -flow_relative_indices parameter, example: 1,15,40. Default value 1 makes optical flow for short-term flow.

--- makeOptFlow.sh
+++ makeOptFlow.sh
@@ -1,5 +1,5 @@
 # Specify the path to the optical flow utility here.
-# Also check line 56 and 59 whether the arguments are in the correct order.
+# Also check line 63 and 66 whether the arguments are in the correct order.
 flowCommandLine="bash run-deepflow.sh"
 
 if [ -z "$flowCommandLine" ]; then
@@ -19,13 +19,15 @@ fi
 
 filePattern=$1
 folderName=$2
-opt_res=$3
-startFrame=${4:-1}
-stepSize=${5:-1}
+startFrame=${3:-1}
+stepSize=${4:-1}
+opt_res=${5:-1}
 
+old_IFS=$IFS
+IFS=',' read -r -a stepSize <<< "$stepSize"
 
 wait_for_file() {
-   filename=$1
+   local filename=$1
    while [ ! -f "$filename" ]; do 
    sleep 1 
    done
@@ -38,32 +40,42 @@ if [ "$#" -le 1 ]; then
    echo -e "\tfilePattern:\tFilename pattern of the frames of the videos."
    echo -e "\toutputFolder:\tOutput folder."
    echo -e "\tstartNumber:\tThe index of the first frame. Default: 1"
-   echo -e "\tstepSize:\tThe step size to create long-term flow. Default: 1"
+   echo -e "\tstepSize:\tThe step size to create long-term flow. May be an array, similar to -flow_relative_indices parameter: 1,15,40 .Default: 1. "
    echo -e "\topt_res:\tResolution for optical flow. Default: 1"
    exit 1
 fi
 
+
+loopWork=1
+
 i=$[$startFrame]
-j=$[$startFrame + $stepSize]
 
 mkdir -p "${folderName}"
 
-while true; do
-  file1=$(printf "$filePattern" "$i")
-  file2=$(printf "$filePattern" "$j")
-  if [ -a $file2 ]; then
-    if [ ! -f ${folderName}/forward_${i}_${j}.flo ]; then
-      eval $flowCommandLine "$file1" "$file2" "${folderName}/forward_${i}_${j}.flo" ${opt_res} &
+while [ $loopWork = 1 ]; do
+  for step in "${stepSize[@]}"; do
+    j=$[ $i - $step ]
+    file1=$(printf "$filePattern" "$i")
+    file2=$(printf "$filePattern" "$j")
+
+    if [ -a $file2 ] && [ -a $file1 ]; then
+      if [ ! -f ${folderName}/forward_${j}_${i}.flo ]; then
+        eval $flowCommandLine "$file2" "$file1" "${folderName}/forward_${j}_${i}.flo" ${opt_res} &
+      fi
+      if [ ! -f ${folderName}/backward_${i}_${j}.flo ]; then
+        eval $flowCommandLine "$file1" "$file2" "${folderName}/backward_${i}_${j}.flo" ${opt_res}
+      fi
+      wait_for_file "${folderName}/forward_${j}_${i}.flo"
+      ./consistencyChecker/consistencyChecker "${folderName}/backward_${i}_${j}.flo" "${folderName}/forward_${j}_${i}.flo" "${folderName}/reliable_${i}_${j}.pgm"
+      ./consistencyChecker/consistencyChecker "${folderName}/forward_${j}_${i}.flo" "${folderName}/backward_${i}_${j}.flo" "${folderName}/reliable_${j}_${i}.pgm"
+    
     fi
-    if [ ! -f ${folderName}/backward_${j}_${i}.flo ]; then
-      eval $flowCommandLine "$file2" "$file1" "${folderName}/backward_${j}_${i}.flo" ${opt_res}
+
+    if [ ! -f $file1 ]; then
+      loopWork=0	
     fi
-    wait_for_file "${folderName}/forward_${i}_${j}.flo"
-    ./consistencyChecker/consistencyChecker "${folderName}/backward_${j}_${i}.flo" "${folderName}/forward_${i}_${j}.flo" "${folderName}/reliable_${j}_${i}.pgm"
-    ./consistencyChecker/consistencyChecker "${folderName}/forward_${i}_${j}.flo" "${folderName}/backward_${j}_${i}.flo" "${folderName}/reliable_${i}_${j}.pgm"
-  else
-    break
-  fi
-  i=$[$i +1]
-  j=$[$j +1]
+    
+  done
+  i=$[$i + 1]
+
 done
